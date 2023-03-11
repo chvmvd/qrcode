50fbebbd837a9fb99068184a1ca7b667695e6488
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-11 13:37:07 +0900

    Create QR code generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QRCode from "qrcode";
 
 function App(): JSX.Element {
-  return <>Hello World!</>;
+  const [inputValue, setInputValue] = useState<string>("sample");
+  const [generatedQRCode, setGeneratedQRCode] = useState<string>("");
+  useEffect(() => {
+    QRCode.toString(
+      inputValue,
+      { type: "svg" },
+      function (err: Error | null | undefined, string) {
+        if (err === null || err === undefined ? false : Boolean(err))
+          throw err as Error;
+        setGeneratedQRCode(string);
+      }
+    );
+  }, [inputValue]);
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <img src={`data:image/svg+xml,${encodeURIComponent(generatedQRCode)}`} />
+    </>
+  );
 }
 
 export default App;
